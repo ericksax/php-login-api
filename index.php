981eaf5a4b05541669bd9fb129ab9c29d24ca211
ericksax <erickfreitas@gmail.com>
2024-04-14 21:09:27 -0300

    feat: set http header

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ $dotenv->load();
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 Routes::execute();
 
